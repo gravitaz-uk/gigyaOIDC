c1e8ba7c33aa6211668da27df1c1b79142f39fa9
holmestm <tim@gravitaz.co.uk>
2019-09-05 13:06:47 +0100

    Keys and secrets go here
    Not production ready

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    $apiKey = ''; 
+    $urlPrefix='https://<my hostname>/oidc';
+    $clientID = 'define RP in Gigya console';
+    $clientSecret='from Gigya console';
+    $partnerSecret='from Gigya console';
+    $redirectURI=$urlPrefix . '/loggedin.php';
+    $screenSet_prefix='Default';
+    $userinfoEndpoint='<from Gigya console>/userinfo';
+    $authorizeEndpoint='<from Gigya console>/authorize';
+    $tokenEndpoint='<from Gigya console>/token';
+?>
